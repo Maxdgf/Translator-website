f60cd9a56e7a2ec0436ab0ec6ec63c690a5519a9
Max <162334963+Maxdgf@users.noreply.github.com>
2024-12-01 14:46:33 +0300

    Update settings.py

--- Translater/settings.py
+++ Translater/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n57a1&b215vr(&2klpzb35widcqjil8-j-v&*pek=qaqlz^zx('
+SECRET_KEY = 'your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
